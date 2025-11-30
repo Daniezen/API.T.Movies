c78bc2065ae1f8985c63a88b67bbbfee6fb76e16
daKaizen7 <daniel0598@hotmail.com>
2025-11-30 16:32:56 -0500

    AutoMapper and DTO's created

---
+++ API.T.Movies/MoviesMapper/Mappers.cs
@@ -0,0 +1,15 @@
+﻿using API.T.Movies.DAL.Dtos.Category;
+using API.T.Movies.DAL.Models;
+using AutoMapper;
+
+namespace API.J.Movies.MoviesMapper
+{
+    public class Mappers : Profile
+    {
+        public Mappers()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryCreateUpdateDto>().ReverseMap();
+        }
+    }
+}
