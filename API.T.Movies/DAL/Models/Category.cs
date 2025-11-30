9f02e1f4086620b2f8444e49e6e59b3eed2d1f51
daKaizen7 <daniel0598@hotmail.com>
2025-11-30 15:01:17 -0500

    Primer Commit: Creación de base de datos y uso de Entity Framework Core

---
+++ API.T.Movies/DAL/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.T.Movies.DAL.Models
+{
+    public class Category : AuditBase
+    {
+        [Required] // Este decorator indica que el campo es obligatorio (no acepta nulls)
+        public string Name { get; set; }
+    }
+}
